9644b7ebc44eb998d741c50b6607d92cea5652c1
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-04 22:48:49 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -1,7 +1,8 @@
-const spawnUnit = Vars.content.getByName(ContentType.unit, "testmod-electricdagger");
+const spawnUnit = Vars.content.getByName(ContentType.unit, "dagger");
 const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
 
 EntityMapping.nameMap.put("testunit4", testunit4Entity);
+
 const testunit4 = extendContent(UnitType, "testunit4", {});
 
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
