7b40170e7f756671becc783daec4b5c9aad19234
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-04 20:37:48 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -1,4 +1,4 @@
-const testunit4 = extendContent(BuilderMinerPayloadUnit, "testunit4", {
+const testunit4 = extendContent(UnitType, "testunit4", {
     load() {
         this.super$load();
         this.region = Core.atlas.find(this.name);
@@ -6,3 +6,6 @@ const testunit4 = extendContent(BuilderMinerPayloadUnit, "testunit4", {
 });
 
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
+
+testunit4.constructor = () => extend(BuilderMinerPayloadUnit, {});
+
