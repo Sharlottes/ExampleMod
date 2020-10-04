c58265dec34d6bce580e72ff8c971f27c01b7b25
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-04 20:58:32 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -6,10 +6,10 @@ const testunit4 = extendContent(UnitType, "testunit4", {
 });
 
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
-testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod"+"-"+"electricdagger", 5 * 60, 8, 4));
-testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod"+"-"+"electricdagger", 5 * 60, -8, 4));
-testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod"+"-"+"electricdagger", 5 * 60, 8, -4));
-testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod"+"-"+"electricdagger", 5 * 60, -8, -4));
+testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod-electricdagger", 5 * 60, 8, 4));
+testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod-electricdagger", 5 * 60, -8, 4));
+testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod-electricdagger", 5 * 60, 8, -4));
+testunit4.abilities.add(new UnitSpawnAbility(Vars.content.getByName(ContentType.unit, "testmod-electricdagger", 5 * 60, -8, -4));
 
 
 testunit4.constructor = () => extend(BuilderMinerPayloadUnit, {});
