729e7bbcda406d46a42e1c1642ab2b570397fdc5
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 21:38:10 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -2,7 +2,8 @@ const healColor = Color.valueOf("98ffa9");
 const spawnUnit = Vars.content.getByName(ContentType.unit, "fortress");
 const spawnUnit2 = Vars.content.getByName(ContentType.unit, "dagger");
 const spawnUnit3 = Vars.content.getByName(ContentType.unit, "pulsar");
-
+const UnitSpawn1 = new UnitSpawnAbility(spawnUnit3, 15 * 60, 0, 0);
+const healField1 = new HealFieldAbility(50, 2 * 60, 20 * 8);
 const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
 EntityMapping.nameMap.put("testunit4", testunit4Entity);
 
@@ -48,9 +49,9 @@ const healIn = new Effect (60, e => {
 });
 
 
-const UnitSpawn1 = new UnitSpawnAbility(spawnUnit3, 15 * 60, 0, 0);
+
 UnitSpawn1.spawnEffect = mendSpawn;
 
 
-//const healField1 = new HealFieldAbility(50, 2 * 60, 20 * 8);
+
 //healField1.healEffect = healIn;
