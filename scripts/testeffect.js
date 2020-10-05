295d151b80cbcd38f4e7b4a5a3ae88a423c17e54
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 11:14:40 +0900

    자바스크립트 수정

--- scripts/testeffect.js
+++ scripts/testeffect.js
@@ -1,6 +1,6 @@
-const testcolor = Color.valueOf("98ffa9");
+const healColor = Color.valueOf("98ffa9");
 
-const testeffect = new Effect(120, e => {
+const mendSpawn = new Effect(120, e => {
     if(!(e.data instanceof UnitType)) {
         return;
     }
@@ -17,4 +17,19 @@ const testeffect = new Effect(120, e => {
 
 });
 
-testeffect.at(0, 0, testcolor);
+const healIn = new Effect (60, e => {
+    Draw.color(e.color);
+
+    e.scaled(8f, e2 -> {
+        Draw.stroke(e2.fout() * 4f);
+        Lines.circle(e2.x, e2.y, 4 + e2.fin() * 27);
+    });
+
+    Draw.stroke(e.fout() * 2f);
+
+    Angles.randLenVectors(e.id, 30, 4f + 40f * e.fin(), (x, y) -> {
+        Draw.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fin() * 4 + 1);
+});
+
+mendSpawn.at(0, 0, healColor);
+healIn.at(0, 0, healColor);
--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -1,18 +1,23 @@
 const fx = require("testeffect");
 const spawnUnit = Vars.content.getByName(ContentType.unit, "fortress");
 const spawnUnit2 = Vars.content.getByName(ContentType.unit, "dagger");
-const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
+const testUnit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
 
-EntityMapping.nameMap.put("testunit4", testunit4Entity);
+EntityMapping.nameMap.put("testunit4", testUnit4Entity);
 
 
 
-const testunit4 = extendContent(UnitType, "testunit4", {});
+const testUnit4 = extendContent(UnitType, "testunit4", {});
+const forceField1 = extendContent(Ability, "ForceFieldAbility", {});
+const healField1 = extendContent(Ability, "HealFieldAbility", {});
 
-testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
+testUnit4.abilities.add(new HealFieldAbility(3, 2 * 60, 10 * 8));
+testUnit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
+testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
+testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
+testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
+testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
 
-testunit4.spawnEffect = fx.testeffect;
+forceField1.spawnEffect = fx.mendSpawn;
+
+healField1.healEffect = fx.healIn;
