824fed11e718208f17892013b43c2d35448b0688
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 19:12:16 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -7,15 +7,15 @@ EntityMapping.nameMap.put("testunit4", testunit4Entity);
 
 const testunit4 = extendContent(UnitType, "testunit4", {});
 
-testunit4.abilities.add(new JavaAdapter(HealFieldAbility, 3, 2 * 60, 10 * 8, {}));
+testunit4.abilities.add(new JavaAdapter(HealFieldAbility, {}, 3, 2 * 60, 10 * 8));
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
 
-const forceField1 = extend(ForceFieldAbility, {});
+const forceField1 = new JavaAdapter(ForceFieldAbility);
 forceField1.spawnEffect = fx.mendSpawn;
 
-const healField1 = extend(HealFieldAbility, {});
+const healField1 = new JavaAdapter(HealFieldAbility);
 healField1.healEffect = fx.healIn;
