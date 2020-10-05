a278244ed737b08288fdb934690ffd79a296df29
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 17:14:59 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -4,16 +4,12 @@ const spawnUnit2 = Vars.content.getByName(ContentType.unit, "dagger");
 
 
 
-const testunit4 = extendContent(UnitType, "testunit4", {});
+const testunit4 = extendContent(BuilderMinerPayloadUnit, "testunit4", {});
 const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
 
-testunit4.constructor = () => extend(BuilderMinerPayloadUnit, {});
 testunit4.constructor = EntityMapping.nameMap.put("testunit4", testunit4Entity);
 
 
-
-const forceField1 = extend(ForceFieldAbility, {});
-const healField1 = extend(HealFieldAbility, {});
 
 testunit4.abilities.add(new HealFieldAbility(3, 2 * 60, 10 * 8));
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
@@ -22,5 +18,8 @@ testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
 
+const forceField1 = extend(ForceFieldAbility, {});
 forceField1.spawnEffect = fx.mendSpawn;
+
+const healField1 = extend(HealFieldAbility, {});
 healField1.healEffect = fx.healIn;
