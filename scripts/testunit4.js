be8f0598acee08259106756c6b659b2db2c2e701
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 21:43:24 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -5,10 +5,9 @@ const spawnUnit3 = Vars.content.getByName(ContentType.unit, "pulsar");
 const UnitSpawn1 = new UnitSpawnAbility(spawnUnit3, 15 * 60, 0, 0);
 const healField1 = new HealFieldAbility(50, 2 * 60, 20 * 8);
 const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
-EntityMapping.nameMap.put("testunit4", testunit4Entity);
-
 const testunit4 = extendContent(UnitType, "testunit4", {});
 
+EntityMapping.nameMap.put("testunit4", testunit4Entity);
 testunit4.abilities.add(new JavaAdapter(HealFieldAbility, {}, 50, 2 * 60, 20 * 8));
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, 18, 7));
