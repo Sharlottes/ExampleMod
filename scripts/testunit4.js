fd7dfc16a33a6e4c1d561908c70d9addd99c7859
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 14:09:10 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -2,21 +2,21 @@ const fx = require("testeffect");
 const spawnUnit = Vars.content.getByName(ContentType.unit, "fortress");
 const spawnUnit2 = Vars.content.getByName(ContentType.unit, "dagger");
 
-const testUnit4 = extendContent(UnitType, "testunit4", {});
-const testUnit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
-EntityMapping.nameMap.put("testunit4", testUnit4);
-EntityMapping.nameMap.put("testunit4", testUnit4Entity);
+const testunit4 = extendContent(UnitType, "testunit4", {});
+const testunit4Entity = prov(() => extend(BuilderMinerPayloadUnit, {}));
+EntityMapping.nameMap.put("testunit4", testunit4);
+EntityMapping.nameMap.put("testunit4", testunit4Entity);
 testunit4.constructor = () => extend(BuilderMinerPayloadUnit, {});
 
 const forceField1 = extend(ForceFieldAbility, {});
 const healField1 = extend(HealFieldAbility, {});
 
-testUnit4.abilities.add(new HealFieldAbility(3, 2 * 60, 10 * 8));
-testUnit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
-testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
-testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
-testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
-testUnit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
+testunit4.abilities.add(new HealFieldAbility(3, 2 * 60, 10 * 8));
+testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
 
 forceField1.spawnEffect = fx.mendSpawn;
 healField1.healEffect = fx.healIn;
