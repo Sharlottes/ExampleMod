1d499ae832e9292092c98849c7293908a7bc3362
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-05 20:39:39 +0900

    자바스크립트 수정

--- scripts/testunit4.js
+++ scripts/testunit4.js
@@ -9,12 +9,12 @@ const testunit4 = extendContent(UnitType, "testunit4", {});
 
 testunit4.abilities.add(new JavaAdapter(HealFieldAbility, {}, 3, 2 * 60, 10 * 8));
 testunit4.abilities.add(new ForceFieldAbility(96, 0.5, 550, 2 * 60));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, 7));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, 18, 7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, 7));
 testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, 18, -7));
-testunit4.abilities.add(new UnitSpawnAbility(spawnUnit, 5 * 60, -18, -7));
+testunit4.abilities.add(new UnitSpawnAbility(spawnUnit2, 5 * 60, -18, -7));
 
-const mendSpawn = new Effect(120, e => {
+const mendSpawn = new Effect (120, e => {
     if(!(e.data instanceof UnitType)) {
         return;
     }
@@ -46,8 +46,8 @@ const healIn = new Effect (60, e => {
     });
 });
 
-const UnitSpawn1 = new UnitSpawnAbility;
+const UnitSpawn1 = new UnitSpawnAbility();
 UnitSpawn1.spawnEffect = mendSpawn;
 
-//const healField1 = new HealFieldAbility;
-//healField1.healEffect = healIn;
+const healField1 = new HealFieldAbility();
+healField1.healEffect = healIn;
