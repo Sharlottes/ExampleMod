a1de7bf9bf614856843812c448935aad92a6eda5
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-06 20:05:19 +0900

    아이템 생성 그래픽 효과 추가, 활성체 아이템 추가

--- scripts/activationmatter-weaver.js
+++ scripts/activationmatter-weaver.js
@@ -1,2 +1,17 @@
+const activationMatterColor = Color.valueOf("a3ddf5");
 const activationMatterWeaver = extendContent(GenericCrafter, "activationmatter-weaver", {});
+
+
+const weaverOut = new Effect (60, e => { //아이템 생성 그래픽 효과
+    Draw.color(activationMatterColor);
+    Lines.stroke(e.fout() * 2);
+    Lines.circle(e.x, e.y, 7 + e.fin() * 8);
+
+    Angles.randLenVectors(e.id, 20, 4f + 20f * e.fin(), (x, y) -> {
+        Draw.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fslope() * 4f + 1f);
+    });
+});
+
+
 activationMatterWeaver.drawer = new DrawWeave();
+activationMatterWeaver.craftEffect = weaverOut;
