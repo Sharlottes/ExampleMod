ffe74c1ad454fc7678f4398e65930953a0b8ba42
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-06 20:07:06 +0900

    자바스크립트 수정

--- scripts/activationmatter-weaver.js
+++ scripts/activationmatter-weaver.js
@@ -7,8 +7,8 @@ const weaverOut = new Effect (60, e => { //아이템 생성 그래픽 효과
     Lines.stroke(e.fout() * 2);
     Lines.circle(e.x, e.y, 7 + e.fin() * 8);
 
-    Angles.randLenVectors(e.id, 20, 4f + 20f * e.fin(), (x, y) -> {
-        Draw.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fslope() * 4f + 1f);
+    Angles.randLenVectors(e.id, 20, 4 + 20 * e.fin(), (x, y) -> {
+        Draw.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), e.fslope() * 4 + 1);
     });
 });
 
