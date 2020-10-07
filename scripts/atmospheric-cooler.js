8a33c810ef8ac8c152f0949da2ba1a05779cd28f
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 13:38:12 +0900

    대기 냉각기 수정

--- scripts/atmospheric-cooler.js
+++ scripts/atmospheric-cooler.js
@@ -1,4 +1,3 @@
-
 const rotateBlock = extend(DrawMixer, {
     draw(entity){
         var rotation = entity.block.rotate ? entity.rotdeg() : 0;
@@ -10,18 +9,18 @@ const rotateBlock = extend(DrawMixer, {
             Draw.alpha(entity.liquids.get(((GenericCrafter)entity.block).outputLiquid.liquid) / entity.block.liquidCapacity);
             Draw.rect(this.liquid, entity.x, entity.y, rotation);
             Draw.color();
-        }
+        };
         Draw.rect(this.top, entity.x, entity.y, rotation);
 
         Draw.rect(entity.block.region, entity.x, entity.y);
         Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 6);
-    }
+    },
     load(block){
-        var rotator = Core.atlas.find(block.name + "-rotator");
-        var liquid = Core.atlas.find(block.name + "-liquid");
-        var top = Core.atlas.find(block.name + "-top");
-        var bottom = Core.atlas.find(block.name + "-bottom");
-    }
+        this.rotator = Core.atlas.find(block.name + "-rotator");
+        this.liquid = Core.atlas.find(block.name + "-liquid");
+        this.top = Core.atlas.find(block.name + "-top");
+        this.bottom = Core.atlas.find(block.name + "-bottom");
+    },
     icons(block){
     return new TextureRegion[]{bottom, rotator, top};
     }
