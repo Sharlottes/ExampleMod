0744090e8096f330093d8a6d6b0b7e061fa277ce
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 12:11:24 +0900

    대기 냉각기 수정

--- scripts/atmospheric-cooler.js
+++ scripts/atmospheric-cooler.js
@@ -1,13 +1,8 @@
-var n = 0;
+
 const rotateBlock = extend(DrawRotator, {
     draw(entity){
         Draw.rect(entity.block.region, entity.x, entity.y);
         Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 2 * Time.delta);
-        n += 2;
-        if(n >= 360){
-            Time.run(90, () => {});
-            var n = 0;
-        };
     }
 });
 const atmosphericCooler = extendContent(GenericCrafter, "atmospheric-cooler", {});
