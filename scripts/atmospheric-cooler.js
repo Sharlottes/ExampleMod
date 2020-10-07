bc3bdb8275de70362eb656561d4fc95b5e5c78dc
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 11:53:47 +0900

    대기 냉각기 수정

--- scripts/atmospheric-cooler.js
+++ scripts/atmospheric-cooler.js
@@ -2,7 +2,8 @@ const rotateBlock = extend(DrawRotator, {
   draw(entity){
     Draw.rect(entity.block.region, entity.x, entity.y);
     Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 2 * Time.delta);
-    var n += 2;
+    var n;
+    n += 2;
     if(n >= 360){
       Time.run(90, () => {});
       n = 0;
