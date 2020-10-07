88161172b2de351cedbd66af10f527e894dcd708
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 20:01:48 +0900

    버그수정/철광 추가

---
+++ scripts/iron.js
@@ -0,0 +1,8 @@
+const ironOre = extend(OreBlock, {});
+const ironItem = extendContent(Item, "iron", {});
+const iron = Vars.content.getByName(ContentType.item, "testmod-iron");
+
+ironOre.localizedName = ironItem.localizedName;
+ironOre.itemDrop = iron;
+ironOre.variants = 3;
+ironOre.mapColor.set(ironItem.color);
