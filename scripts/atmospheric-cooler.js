a6a59c32a23334becb47730c900cb5c350013eed
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 15:19:02 +0900

    대기 냉각기 수정

--- scripts/atmospheric-cooler.js
+++ scripts/atmospheric-cooler.js
@@ -3,20 +3,13 @@ const rotateBlock = extend(DrawMixer, {
         const rotation = entity.block.rotate ? entity.rotdeg() : 0;
 
         Draw.rect(this.bottom, entity.x, entity.y, rotation);
-
-        if(entity.liquids.total() > 0.001){
-            Draw.color(entity.block.outputLiquid.liquid.color);
-            Draw.alpha(entity.liquids.get(entity.block.outputLiquid.liquid) / entity.block.liquidCapacity);
-            Draw.rect(this.liquid, entity.x, entity.y, rotation);
-            Draw.color();
-        }
         Draw.rect(entity.block.region, entity.x, entity.y);
         Draw.rect(this.rotator, entity.x, entity.y, entity.totalProgress * 6);
         Draw.rect(this.top, entity.x, entity.y, rotation);
     },
     load(block){
         this.rotator = Core.atlas.find(block.name + "-rotator");
-        this.liquid = Core.atlas.find(block.name + "-liquid");
+
         this.top = Core.atlas.find(block.name + "-top");
         this.bottom = Core.atlas.find(block.name + "-bottom");
     },
