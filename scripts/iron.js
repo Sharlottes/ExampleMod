92fc2477a389906628b4538c400f746a18fd3f21
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-07 20:48:38 +0900

    자바스크립트 수정

--- scripts/iron.js
+++ scripts/iron.js
@@ -1,8 +1,8 @@
 const ironOre = extend(OreBlock, {});
-const ironItem = extendContent(Item, "iron", {});
+//const ironItem = extendContent(Item, "iron", {});
 const iron = Vars.content.getByName(ContentType.item, "testmod-iron");
 
-ironOre.localizedName = ironItem.localizedName;
+//ironOre.localizedName = ironItem.localizedName;
 ironOre.itemDrop = iron;
 ironOre.variants = 3;
-ironOre.mapColor.set(ironItem.color);
+//ironOre.mapColor.set(ironItem.color);
