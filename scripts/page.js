413913a9043a472d619893a1db998411fb937976
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-09 18:59:34 +0900

    페이지 수정

--- scripts/page.js
+++ scripts/page.js
@@ -19,6 +19,10 @@ try {
 		"2. done"
 	]);
 
+    print(rtfm.currentPage);
+    print(rtfm.pages);
+    print(rtfm.dialog);
+    print(rtfm);
 	/* Sections for bigger mods.
 		 Files have content as null.
 		 Sections have content as an object, not an array. *//*
