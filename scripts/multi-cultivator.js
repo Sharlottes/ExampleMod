22648a577289fbca059ec851f2e7260a15781397
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-09 20:43:59 +0900

    페이지 수정/ui 추가

--- scripts/multi-cultivator.js
+++ scripts/multi-cultivator.js
@@ -1,2 +1,16 @@
 const multiCultivator = extendContent(GenericCrafter,"multi-cultivator",{});
+const rtfmUI = require("rtfm/library");
+
+
 multiCultivator.drawer = new DrawAnimation();
+multiCultivator.buildType = () => {
+    return extendContent(GenericCrafter.GenericCrafterBuild, multiCultivator, {
+        buildConfiguration(table){
+            this.super$buildConfiguration(table);
+            table.button(Icon.star, () => {
+                this.configure(new Integer(1));
+                rtfmUI.showPage("$block.floor", false);
+            }).size(40);
+        }
+    });
+}
