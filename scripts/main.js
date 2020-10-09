8e2a22b6b5e505b95343bf80e752ad31175eb218
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-09 15:44:03 +0900

    테크트리 수정

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ require("testunit4");
 require("activationmatter-weaver");
 require("multi-cultivator");
 require("atmospheric-cooler");
-require("iron");
-require("ore-iron");
-//require("test");
+//require("iron");
+//require("ore-iron");
+require("test");
 //require("techtree");
 
 
