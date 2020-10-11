619f3a93d22f90555892b1f9423f92dde8d62476
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-11 13:31:39 +0900

    hh

--- scripts/power-processor.js
+++ scripts/power-processor.js
@@ -4,13 +4,12 @@ const Integer = java.lang.Integer;
 powerProcessor.configurable = true;
 powerProcessor.buildType = () => {
   return extendContent(LogicBlock.LogicBuild, powerProcessor, {
-
     buildConfiguration(table){
-        this.super$buildConfiguration(table);
-        table.button(Icon.star, () => {
-            this.configure(new Integer(1));
-            rtfmUI.showPage("$block.mod.title", true);
-        }).size(40);
+      this.super$buildConfiguration(table);
+      table.button(Icon.star, () => {
+          this.configure(new Integer(1));
+          rtfmUI.showPage("$block.mod.title", true);
+      }).size(40);
     }
   });
 }
