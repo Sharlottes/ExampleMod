c06dd435c1a01d0acc23170b8d2757aaaf084c0e
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-11 13:25:38 +0900

    h

--- scripts/page.js
+++ scripts/page.js
@@ -1,17 +1,6 @@
 try {
 	const rtfm = require("rtfm/library");
-
-	/* Lone pages, good for small mods */
-	// Page is stored in manuals/Epic file page
-
-    /*
-	rtfm.addPage("test");
-	rtfm.addPage("test2", [
-		"# Steps",
-		"1. stop chaining routers",
-		"2. done"
-	]);
-    */
+	/*
   rtfm.addSection("$block.mod.title", {
         "$block.mod.ironWall": null,
         "$block.mod.multicultivator": null
@@ -20,42 +9,44 @@ try {
         "$block.floor": null,
         "$block.cliff": null
     });
+
 	for (var block of [Blocks.router, Blocks.sorter, Blocks.duo]) {
-	    rtfm.addPage(block.localizedName, [
-		    "then, this is page. Right?",
-		    () => new Label(prov(() => Mathf.random(0, 10) + "")),
-		    "so.. can i add page under new Label?"
-	    ], rtfm.pages["$block.mod.title"]);
+    rtfm.addPage(block.localizedName, [
+	    "then, this is page. Right?",
+    	() => new Label(prov(() => Mathf.random(0, 10) + "")),
+	    "so.. can i add page under new Label?",
+			"bruh.. this is array. don't be dump anymore"
+    ], rtfm.pages["$block.mod.title"]);
 	};
-	/* Sections for bigger mods.
-		 Files have content as null.
-		 Sections have content as an object, not an array. *//*
-	rtfm.addSection("OP Walls", {
-		"$op-walls.epic-bundle": [
-			"# Bundle page",
-			"This page is [coral]sick[] and uses a bundle in its title.",
-		],
-		"Page 2 from a file": null
-	});
-    */
-	/* Custom elements with the default builder.
-		 Cannot be done with a file. *//*
-	rtfm.addPage("hack zone", [
-		"You are now hack",
-		new Label(prov(() => Mathf.random(0, 10) + ""))
-	]);
-    */
-	/* Entirely custom page! */
-	rtfm.addPage("s p a c e", {
-		build(page) {
-			page.table.table(t => {
-				t.button("[yellow]egg", Icon.trash, () => {
-					print("Execute router 66");
-				}).center().size(500, 40);
-			}).size(4000);
+	*/
+	const blockArr = Vars.content.blocks().toArray();
+	for (var h=0; h<blockArr.length; h++){
+		var front = blockArr[h];
+		if(front == null) return; //아무것도 없는데 뭘 하겠다는거요?
+		var str = ""; //형변환 유도를 위한 빈 문자열 선언/정의
+		const arr = Object.keys(front); //대상 키-값쌍의 키들을 수집해서 배열로 반환
+		arr.sort(); //배열 인수들을 생성순서대로 정렬
+
+		for(var i=0; i<arr.length; i++){ //arr 배열 길이만큼 반복
+			if(arr[i] === ""){ //arr 인수가 문자열 "" 일 경우
+				continue; //반복문 반영구 정지
+			}
+			try{
+				if((typeof front[arr[i]]) === "function"){ //arr 인수를 매개변수로 가진? front[]가 문자열 function일 경우
+					continue; //반복문 반영구 정지
+				};
+				str += arr[i]; //빈 문자열에 arr 인수 추가. 형변환 유도
+				str += (front[arr[i]] == null)?(": [lightgray]null[]"):(": [accent]" + front[arr[i]] + "[]");//arr 인수가 추가된 문자열에서 arr 인수를 매개변수로 가진? front[]가 null일 경우, : null을 추가. null이 아닐 경우, front[]를 추가
+				if(i < arr.length - 1) str += "\n"; //arr 배열의 길이-1가 i보다 클 경우, \n 까지 덧붙임. 줄 바꿔쓰기
+			}
+			catch(ignore){ //예외 발생시 실행문
+
+			}
 		}
-	}, rtfm.pages["$block.title"]);
-	// Still here? Check out RTFM Docs / API or scripts/library.js and get your hands real dirty.
+	rtfm.addPage("$block.title", {
+		"~"+str
+	});
+	}
 } catch (e) {
 	// Message here
 	Log.warn("Please install [#00aaff]DeltaNedas/rtfm[] to view OP Walls's manual pages.");
--- scripts/power-processor.js
+++ scripts/power-processor.js
@@ -3,13 +3,14 @@ const powerProcessor = extendContent(LogicBlock,"power-processor",{});
 const Integer = java.lang.Integer;
 powerProcessor.configurable = true;
 powerProcessor.buildType = () => {
-    return extendContent(LogicBlock.LogicBuild, powerProcessor, {
-        buildConfiguration(table){
-            this.super$buildConfiguration(table);
-            table.button(Icon.star, () => {
-                this.configure(new Integer(1));
-                rtfmUI.showPage("$block.mod.title", true);
-            }).size(40);
-        }
-    });
+  return extendContent(LogicBlock.LogicBuild, powerProcessor, {
+
+    buildConfiguration(table){
+        this.super$buildConfiguration(table);
+        table.button(Icon.star, () => {
+            this.configure(new Integer(1));
+            rtfmUI.showPage("$block.mod.title", true);
+        }).size(40);
+    }
+  });
 }
