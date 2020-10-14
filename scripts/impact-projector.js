c9b64e7be66007b0fbd644750fd77d21a58fbca7
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-14 18:33:09 +0900

    what's wrong with me

--- scripts/impact-projector.js
+++ scripts/impact-projector.js
@@ -5,13 +5,12 @@ impactProjector.buildType = () => extendContent(ForceProjector.ForceBuild, impac
         if(!this.broken){
             var radius = this.realRadius();
 
-            //Draw.z(Layer.shields);
             var flashThreshold = 0.46;
             var flash = 1 + ((this.phaseHeat - flashThreshold) / (1 - flashThreshold)) * 5.4;
             flash += flash * Time.delta;
-            Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.clamp(this.hit));
+            //Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.clamp(this.hit));
             Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.absin(flash, 9, 1));
-
+            Draw.z(Layer.shields);
             if(Core.settings.getBool("animatedshields")){
                 Fill.poly(this.x, this.y, 6, radius);
             }else{
@@ -23,12 +22,6 @@ impactProjector.buildType = () => extendContent(ForceProjector.ForceBuild, impac
                 Draw.reset();
             }
         }
-        Draw.z(Layer.shields);
         Draw.reset();
     }
 });
-var flashThreshold = 0.46;
-var flash = 1 + ((this.phaseHeat - flashThreshold) / (1 - flashThreshold)) * 5.4;
-flash += flash * Time.delta;
-Draw.color(Color.red, Color.yellow, Mathf.absin(flash, 9, 1));
-Draw.alpha(0.6);
