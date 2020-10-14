816afdeb0523fc92e0db8267e57cfdb73de4f3e9
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-14 18:41:39 +0900

    h

--- scripts/impact-projector.js
+++ scripts/impact-projector.js
@@ -9,7 +9,7 @@ impactProjector.buildType = () => extendContent(ForceProjector.ForceBuild, impac
             var flash = 1 + ((this.phaseHeat - flashThreshold) / (1 - flashThreshold)) * 5.4;
             flash += flash * Time.delta;
             //Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.clamp(this.hit));
-            Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.absin(flash, 9, 1));
+            Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.8), this.team.color.cpy().mul(1, 0.25, 0.25, 0.5), Mathf.absin(flash, 9, 1));
             Draw.z(Layer.shields);
             if(Core.settings.getBool("animatedshields")){
                 Fill.poly(this.x, this.y, 6, radius);
