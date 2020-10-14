b223d372d781c8a91c6eea86f8f7009342404496
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-14 17:36:56 +0900

    test3

--- scripts/impact-projector.js
+++ scripts/impact-projector.js
@@ -2,18 +2,18 @@ const impactProjector = new ForceProjector("impact-projector");
 impactProjector.consumes.add(new ConsumeLiquidFilter(liquid => liquid.temperature <= 1 && liquid.flammability < 1.3, 0.5)).boost().update(false);
 impactProjector.buildType = () => extendContent(ForceProjector.ForceBuild, impactProjector, {
     drawShield(){
-        if(!impactProjector.broken){
-            var radius = impactProjector.realRadius();
+        if(!this.broken){
+            var radius = this.realRadius();
 
             Draw.z(Layer.shields);
 
-            Draw.color(team.color.mul(1, 1, 1, 0.5), Color.white, Mathf.clamp(impactProjector.hit));
+            Draw.color(team.color.cpy().mul(1, 1, 1, 0.5), Color.white.cpy(), Mathf.clamp(this.hit));
 
             if(Core.settings.getBool("animatedshields")){
                 Fill.poly(this.x, this.y, 6, radius);
             }else{
                 Lines.stroke(1.5);
-                Draw.alpha(0.09 + Mathf.clamp(0.08 * impactProjector.hit));
+                Draw.alpha(0.09 + Mathf.clamp(0.08 * this.hit));
                 Fill.poly(this.x, this.y, 6, radius);
                 Draw.alpha(1);
                 Lines.poly(this.x, this.y, 6, radius);
