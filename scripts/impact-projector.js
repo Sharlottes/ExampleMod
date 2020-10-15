a5285b50ad4cd4de6ed0723620142582d03de68d
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-15 19:07:25 +0900

    h

--- scripts/impact-projector.js
+++ scripts/impact-projector.js
@@ -8,7 +8,7 @@ impactProjector.buildType = () => extendContent(ForceProjector.ForceBuild, impac
             var flash = 1 + ((this.phaseHeat - flashThreshold) / (1 - flashThreshold)) * 5.4;
             flash += flash * Time.delta;
             //Draw.color(this.team.color.cpy().mul(1, 1, 1, 0.6), Color.white.cpy(), Mathf.clamp(this.hit));
-            Draw.color(this.team.color.cpy(), this.team.color.cpy().mul(1, 0.25, 0.25, 0.5), Mathf.absin(flash, 9, 1));
+            Draw.color(this.team.color.cpy(), this.team.color.cpy().mul(1, 0.25, 0.25, 1), Mathf.absin(flash, 9, 1));
             Draw.alpha(0.6);
             Draw.z(Layer.shields);
             if(Core.settings.getBool("animatedshields")){
--- scripts/power-processor.js
+++ scripts/power-processor.js
@@ -1,15 +1,20 @@
-const rtfmUI = require("rtfm/library");
 const powerProcessor = extendContent(LogicBlock,"power-processor",{});
-const Integer = java.lang.Integer;
-powerProcessor.configurable = true;
-powerProcessor.buildType = () => {
-  return extendContent(LogicBlock.LogicBuild, powerProcessor, {
-    buildConfiguration(table){
-      this.super$buildConfiguration(table);
-      table.button(Icon.star, () => {
-          this.configure(new Integer(1));
-          rtfmUI.showPage("$block.mod.title", true);
-      }).size(40);
-    }
-  });
+
+try{
+  const rtfmUI = require("rtfm/library");
+  const Integer = java.lang.Integer;
+  powerProcessor.configurable = true;
+  powerProcessor.buildType = () => {
+    return extendContent(LogicBlock.LogicBuild, powerProcessor, {
+      buildConfiguration(table){
+        this.super$buildConfiguration(table);
+        table.button(Icon.star, () => {
+            this.configure(new Integer(1));
+            rtfmUI.showPage("$block.mod.title", true);
+        }).size(40);
+      }
+    });
+  }
+} catch(e) {
+  Log.warn("Please install [#00aaff]DeltaNedas/rtfm[] to view OP Walls's manual pages.");
 }
