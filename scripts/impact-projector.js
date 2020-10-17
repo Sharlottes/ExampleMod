13c9db134ff94f2059f797bd89703bc9f4ebcf56
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-10-17 11:35:20 +0900

    why is this undefined?

--- scripts/impact-projector.js
+++ scripts/impact-projector.js
@@ -5,12 +5,12 @@ const impactProjector = new JavaAdapter(ForceProjector, {
     Draw.color(Pal.gray);
     Lines.stroke(3);
     Lines.circle(x * Vars.tilesize + this.offset, y * Vars.tilesize + this.offset, this.radius);
-    Draw.color(this.team.color.cpy());
+    Draw.color(this.player.team().color.cpy());
     Lines.stroke(1);
     Lines.circle(x * Vars.tilesize + this.offset, y * Vars.tilesize + this.offset, this.radius);
     Lines.stroke(3);
     Lines.circle(x * Vars.tilesize + this.offset, y * Vars.tilesize + this.offset, this.radius + this.phaseRadiusBoost);
-    Draw.color(this.team.color.cpy().mul(1, 0.25, 0.25, 1));
+    Draw.color(this.player.team().color.cpy().mul(1, 0.25, 0.25, 1));
     Lines.stroke(1);
     Lines.circle(x * Vars.tilesize + this.offset, y * Vars.tilesize + this.offset, this.radius + this.phaseRadiusBoost);
     Draw.color();
