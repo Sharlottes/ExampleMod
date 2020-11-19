7deacd9aaaff74e3c5dfc4136dd953803198ae44
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-11-19 18:50:36 +0900

    h

--- scripts/blocks/coredvalut.js
+++ scripts/blocks/coredvalut.js
@@ -1,6 +1,13 @@
 const coredvalut = extendContent(CoreBlock, "core-valut", {
     canReplace(other){
         //YOU CAN REPLACE STORAGEBLOCKS AHYES
-        return this.super$canReplace(other) || (other instanceof StorageBlock);
-    }
+        return this.super$canReplace(other) || (other instanceof StorageBlock && coredvalut.size >= other.size));
+    },
+    canPlaceOn(Tile tile, Team team){
+    if(tile == null) return false;
+    var core = team.core();
+    //must have all requirements
+    if(core == null || (!Vars.state.rules.infiniteResources && !core.items.has(coredvalut.requirements))) return false;
+    return tile.block() instanceof CoreBlock && coredvalut.size >= tile.block().size;
+}
 });
