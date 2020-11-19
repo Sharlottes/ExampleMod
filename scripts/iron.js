184f497630fe020ca1cf64906f8eb254ad494498
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-11-19 17:53:16 +0900

    bugggfix, clean up

--- scripts/iron.js
+++
@@ -1,3 +0,0 @@
-const irons = new Item("iron", Color.valueOf("b0bac0"));
-irons.cost = 2;
-irons.hardness = 5;
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,27 @@
-require("testunit4");
-require("activationmatter-weaver");
-require("multi-cultivator");
-require("atmospheric-cooler");
-//require("iron");
-//require("ore-iron");
-require("test");
-//require("techtree");
-require("page");
-require("power-processor");
-require("impact-projector");
+//blocks
+require("blocks/activationmatter-weaver");
+require("blocks/atmospheric-cooler");
+require("blocks/impact-projector");
+require("blocks/multi-cultivator");
+require("blocks/power-processor");
+
+//experiement
+require("experiement/page");
+require("experiement/oregen");
+
+//units
+require("units/testunit4");
+
+
+
+
+
+
 
 /*
-credit({
+credit(){
   younggam: "answering tons of question",
   deltaNedas: "refer to routorio, rtfm",
   sk7725: "answering tons of question, refer to testers"
+}
 */
