213c9afefac9992d740593eeef07e99c2fa61d45
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-11-28 19:09:23 +0900

    ahhhhhhhhhhh

--- scripts/units/electricmace.js
+++ scripts/units/electricmace.js
@@ -1,5 +1,5 @@
-const electricMaceEntity = prov(() => extend(MechUnit, {}));
-EntityMapping.nameMap.put("electricmace", electricMaceEntity);
-const electricMace = extendContent(UnitType, "electricmace", {});
+const electricFlareEntity = prov(() => extend(MechUnit, {}));
+EntityMapping.nameMap.put("electricflare", electricFlareEntity);
+const electricFlare = extendContent(UnitType, "electricflare", {});
 
-electricMace.abilities.add(new MoveLightningAbility(20, 16, 1, 2.5, 5, Pal.surge));
+electricMace.abilities.add(new MoveLightningAbility(20, 16, 1, 2, 3, Pal.surge));
