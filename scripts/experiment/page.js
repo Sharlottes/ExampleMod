f043d98c68298646638763599713d3e2807bd1ac
Sharlotte <60801210+sharlotte-mobile@users.noreply.github.com>
2020-12-08 13:02:58 +0900

    Update page.js

--- scripts/experiment/page.js
+++ scripts/experiment/page.js
@@ -1,178 +1,216 @@
 Events.on(ClientLoadEvent, () => { //thx Qmel
-		try {
-			const rtfm = require("rtfm/library");
+    try {
+      const rtfm = require("rtfm/library");
 
-		  rtfm.addSection("$block.mod.title", {
-		        "$block.mod.ironWall": null,
-		        "$block.mod.multicultivator": null
-		    });
+      rtfm.addSection("$block.mod.title", {
+            "$block.mod.ironWall": null,
+            "$block.mod.multicultivator": null
+        });
 
-			rtfm.addSection("$block.title", {});
-			rtfm.addSection("$unit.title", {});
-			rtfm.addSection("$item.title", {});
-			rtfm.addSection("$liquid.title", {});
-			rtfm.addSection("$sector.title", {});
-			rtfm.addSection("$planet.title", {});
+      rtfm.addSection("$block.title", {});
+      rtfm.addSection("$unit.title", {});
+      rtfm.addSection("$item.title", {});
+      rtfm.addSection("$liquid.title", {});
+      rtfm.addSection("$sector.title", {});
+      rtfm.addSection("$planet.title", {});
 
-			for (var block of [Blocks.router, Blocks.sorter, Blocks.duo]) {
-			    rtfm.addPage(block.localizedName, [
-					    "then, this is page. Right?",
-				    	() => new Label(prov(() => Mathf.random(0, 10) + "")),
-					    "so.. can i add page under new Label?",
-							"bruh.. this is array. don't be dump anymore"
-			    ], rtfm.pages["$block.mod.title"]);
-			}; //thx ilya
+    
+    
+      rtfm.addPage(Blocks.router.localizedName, [
+          "random go brr",
+          () => new Label(() => Mathf.random(0, 10) + "")              
+      ], rtfm.pages["$block.mod.title"]);//thx ilya
 
 
 
-			const blockArr = Vars.content.blocks().toArray();
-			for(var block of blockArr){ //thx sk
-				var front = block;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+      const blockArr = Vars.content.blocks().toArray();
+      const unitArr = Vars.content.units().toArray();
+      const itemArr = Vars.content.items().toArray();
+      const liquidArr = Vars.content.liquids().toArray();
+      const sectorArr = Vars.content.sectors().toArray();
+      const planetArr = Vars.content.planets().toArray();
+      
+      const scanPage = function(objArr, bundleName){
+          for(var selectedContent of objArr){ //thx sk
+          
+              var front = selectedContent;
+              if(front == null) break;
+              var str = "~";
+              const arr = Object.keys(front);
+              arr.sort();
+              
+              for(var i=0; i<arr.length; i++){
+                  if(arr[i] === "") continue;
+                  try{
+                  if((typeof front[arr[i]]) === "function") continue;
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+                      str += arr[i];
+                      str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+                      if(i < arr.length - 1) str += "\n";
+                  }catch(ignore){}
+              }
+              var centeralStr = "~"+str;
+              rtfm.addPage(selectedContent.localizedName, ["~{"+selectedContent.name+"}", "# "+selectedContent.localizedName, "", centeralStr], rtfm.pages["$"+bundleName+".title"]);
+          }
+      }
+      
+      scanPage(blockArr, "block");
+      scanPage(unitArr, "unit");
+      scanPage(itemArr, "item");
+      scanPage(liquidArr, "liquid");
+      scanPage(planetArr, "planet");
+      scanPage(sectorArr, "sector");
+      
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      /*
+      for(var block of blockArr){ //thx sk
+        var front = block;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(block.localizedName, ["~{"+block.name+"}", "# "+block.localizedName, "", centeralStr], rtfm.pages["$block.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-			const unitArr = Vars.content.units().toArray();
-			for(var unit of unitArr){
-				var front = unit;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+        rtfm.addPage(block.localizedName, ["~{"+block.name+"}", "# "+block.localizedName, "", centeralStr], rtfm.pages["$block.title"]);
+      }
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      
+      for(var unit of unitArr){
+        var front = unit;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(unit.localizedName, ["~{"+unit.name+"}", "# "+unit.localizedName, "", centeralStr], rtfm.pages["$unit.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-			const itemArr = Vars.content.items().toArray();
-			for(var item of itemArr){
-				var front = item;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+        rtfm.addPage(unit.localizedName, ["~{"+unit.name+"}", "# "+unit.localizedName, "", centeralStr], rtfm.pages["$unit.title"]);
+      }
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      
+      for(var item of itemArr){
+        var front = item;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(item.localizedName, ["~{item-"+item.name+"}", "# "+item.localizedName, "", centeralStr], rtfm.pages["$item.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-			const liquidArr = Vars.content.liquids().toArray();
-			for(var liquid of liquidArr){
-				var front = liquid;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+        rtfm.addPage(item.localizedName, ["~{item-"+item.name+"}", "# "+item.localizedName, "", centeralStr], rtfm.pages["$item.title"]);
+      }
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      
+      for(var liquid of liquidArr){
+        var front = liquid;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(liquid.localizedName, ["~{liquid-"+liquid.name+"}", "# "+liquid.localizedName, "", centeralStr], rtfm.pages["$liquid.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-			const sectorArr = Vars.content.sectors().toArray();
-			for(var sector of sectorArr){
-				var front = sector;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+        rtfm.addPage(liquid.localizedName, ["~{liquid-"+liquid.name+"}", "# "+liquid.localizedName, "", centeralStr], rtfm.pages["$liquid.title"]);
+      }
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      
+      for(var sector of sectorArr){
+        var front = sector;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(sector.localizedName, ["~{zone-"+sector.name+"}", "# "+sector.localizedName, "", centeralStr], rtfm.pages["$sector.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-			const planetArr = Vars.content.planets().toArray();
-			for(var planet of planetArr){
-				var front = planet;
-				if(front == null) break;
-				var str = "";
-				const arr = Object.keys(front);
-				arr.sort();
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
 
-				for(var i=0; i<arr.length; i++){
-					if(arr[i] === "") continue;
-					try{
-						if((typeof front[arr[i]]) === "function") continue;
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
 
-						str += arr[i];
-						str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+        rtfm.addPage(sector.localizedName, ["~{zone-"+sector.name+"}", "# "+sector.localizedName, "", centeralStr], rtfm.pages["$sector.title"]);
+      }
 
-						if(i < arr.length - 1) str += "\n";
-					}catch(ignore){}
-				}
-				var centeralStr = "~"+str;
+      
+      for(var planet of planetArr){
+        var front = planet;
+        if(front == null) break;
+        var str = "";
+        const arr = Object.keys(front);
+        arr.sort();
 
-				rtfm.addPage(planet.localizedName, ["~{planet-"+planet.name+"}", "# "+planet.localizedName, "", centeralStr], rtfm.pages["$planet.title"]);
-			}
+        for(var i=0; i<arr.length; i++){
+          if(arr[i] === "") continue;
+          try{
+            if((typeof front[arr[i]]) === "function") continue;
 
-		} catch (e) {
-			Log.warn("Please install [#00aaff]DeltaNedas/rtfm[] to view key: value pages.");
-			print(e);
-			print(e.trace);
-		}
+            str += arr[i];
+            str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+
+            if(i < arr.length - 1) str += "\n";
+          }catch(ignore){}
+        }
+        var centeralStr = "~"+str;
+
+        rtfm.addPage(planet.localizedName, ["~{planet-"+planet.name+"}", "# "+planet.localizedName, "", centeralStr], rtfm.pages["$planet.title"]);
+      }
+      */
+    } catch (e) {
+      Log.warn("Please install [#00aaff]DeltaNedas/rtfm[] to view key: value pages.");
+      print(e);
+      print(e.trace);
+    }
 
 });
