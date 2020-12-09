e134ce9bcbfb374f6c08fda5a4b8b96f6ac18fd6
sharlotte-mobile <60801210+sharlotte-mobile@users.noreply.github.com>
2020-12-09 21:54:57 +0900

    1.4.6v
    clean up, scan weather

--- scripts/experiment/page.js
+++ scripts/experiment/page.js
@@ -8,6 +8,7 @@ Events.on(ClientLoadEvent, () => { //thx Qmel
       rtfm.addSection("$liquid.title", {});
       rtfm.addSection("$sector.title", {});
       rtfm.addSection("$planet.title", {});
+      rtfm.addSection("$weather.title", {});
       rtfm.addSection("$block.mod.title", {});
 
       rtfm.addPage(Blocks.router.localizedName, [
@@ -21,46 +22,60 @@ Events.on(ClientLoadEvent, () => { //thx Qmel
       const liquidArr = Vars.content.liquids().toArray();
       const sectorArr = Vars.content.sectors().toArray();
       const planetArr = Vars.content.planets().toArray();
+      const weatherArr = Vars.content.getBy(ContentType.weather).toArray();
 
       const scanPage = function(objArr, bundleName){
           const vanilaSection = rtfm.addSection("$vanila.title", {}, rtfm.pages["$"+bundleName+".title"]);
           var once = false;
 
           for(var selectedContent of objArr){ //thx sk
-              var isMod = false;
-              var front = selectedContent;
-              if(front == null) break;
-              var str = "~";
-              var arr = Object.keys(front);
-              arr.sort();
+              try{
+                  var isMod = false;
+                  var front = selectedContent;
+                  if(front == null) break;
+                  var str = "~";
+                  var arr = Object.keys(front);
+                  arr.sort();
 
-              if(selectedContent.minfo.mod != null){
-                  isMod = true;
-                  if(contentOwner != selectedContent.minfo.mod.meta.displayName){
-                      once = false;
+                  if(selectedContent.minfo.mod != null){
+                      isMod = true;
+                      if(contentOwner != selectedContent.minfo.mod.meta.displayName) once = false;
+                      if(!once){
+                          var contentOwner = selectedContent.minfo.mod.meta.displayName;
+                          var modSection = rtfm.addSection(selectedContent.minfo.mod.meta.displayName, {}, rtfm.pages["$"+bundleName+".title"]);
+                          once = true;
+                      }
                   }
-                  if(!once){
-                      var contentOwner = selectedContent.minfo.mod.meta.displayName;
-                      var modSection = rtfm.addSection(selectedContent.minfo.mod.meta.displayName, {}, rtfm.pages["$"+bundleName+".title"]);
-                      once = true;
+
+                  for(var i=0; i<arr.length; i++){
+                      if(arr[i] === "") continue;
+                      try{
+                          if((typeof front[arr[i]]) === "function") continue;
+                          str += arr[i];
+                          str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
+
+                          if(i < arr.length - 1) str += "\n~";
+                      }catch(ignored){}
                   }
-              }
 
-              for(var i=0; i<arr.length; i++){
-                  if(arr[i] === "") continue;
-                  try{
-                      if((typeof front[arr[i]]) === "function") continue;
-                      str += arr[i];
-                      str += (front[arr[i]] == null)?(": [lightgray]null[]"):((typeof front[arr[i]]) === "object")?(": [coral]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "number")?(": [sky]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "boolean")?(": [stat]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "undefined")?(": [darkgray]" + front[arr[i]] + "[]"):((typeof front[arr[i]]) === "string")?(": [green]" + front[arr[i]] + "[]"):(": [accent]" + front[arr[i]] + "[]");
-
-                      if(i < arr.length - 1) str += "\n~";
-                  }catch(ignore){}
-              }
-
-	      if(isMod && selectedContent.minfo.mod != null){
-                  rtfm.addPage(selectedContent.localizedName, ["~" + "{" + ((objArr == itemArr) ? selectedContent.minfo.mod.meta.name + "-item-" : ((objArr == liquidArr) ? selectedContent.minfo.mod.meta.name + "-liquid-" : ((objArr == sectorArr) ? selectedContent.minfo.mod.meta.name + "-zone-" : ((objArr == planetArr) ? "-planet-" : "")))) + selectedContent.name + "}", "# "+selectedContent.localizedName, "", str], modSection);
-              }else{
-	       	  rtfm.addPage(selectedContent.localizedName, ["~" + "{" + ((objArr == itemArr) ? "item-" : ((objArr == liquidArr) ? "liquid-" : ((objArr == sectorArr) ? "testmod-zone-" : ((objArr == planetArr) ? "testmod-planet-" : "")))) + selectedContent.name + "}", "# "+selectedContent.localizedName, "", str], vanilaSection);
+                  if(isMod && selectedContent.minfo.mod != null){
+                      rtfm.addPage(selectedContent.localizedName, [
+                          (objArr == weatherArr) ? "" : "~{" + ((objArr == itemArr) ? selectedContent.minfo.mod.meta.name + "-item-" : ((objArr == liquidArr) ? selectedContent.minfo.mod.meta.name + "-liquid-" : ((objArr == sectorArr) ? selectedContent.minfo.mod.meta.name + "-zone-" : ((objArr == planetArr) ? "-planet-" : "")))) + selectedContent.name + "}",
+                          "#"+selectedContent.localizedName,
+                          "",
+                          str
+                      ], modSection);
+                  }else{
+           	          rtfm.addPage(selectedContent.localizedName, [
+                          (objArr == weatherArr) ? "" : "~{" + ((objArr == itemArr) ? "item-" : ((objArr == liquidArr) ? "liquid-" : ((objArr == sectorArr) ? "testmod-zone-" : ((objArr == planetArr) ? "testmod-planet-" : "")))) + selectedContent.name + "}",
+                          "# "+selectedContent.localizedName,
+                          "",
+                          str
+                      ], vanilaSection);
+                  }
+              }catch(errorrrr){
+                  print(errorrrr);
+                  continue;
               }
           }
       }
@@ -71,6 +86,8 @@ Events.on(ClientLoadEvent, () => { //thx Qmel
       scanPage(liquidArr, "liquid");
       scanPage(planetArr, "planet");
       scanPage(sectorArr, "sector");
+      scanPage(weatherArr, "weather");
+
 
     } catch (e) {
       Log.warn("Please install [#00aaff]DeltaNedas/rtfm[], [#00aaff]Deltanedas/ui-lib[] to view scan pages.");
